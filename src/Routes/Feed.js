e9ec99550efcbc19229e47f4097051505b048112
aispark <aispark@paran.com>
2019-10-23 23:10:00 +0900

    7.1 Post Component part One

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -3,6 +3,7 @@ import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
 import Loader from "../Components/Loader";
 import styled from "styled-components";
+import Post from "../Components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -44,6 +45,25 @@ const Wrapper = styled.div`
 
 export default () => {
   const { data, loading } = useQuery(FEED_QUERY);
-  console.log(data, loading);
-  return <Wrapper>{loading && <Loader></Loader>}</Wrapper>;
+  console.log(data);
+  return (
+    <Wrapper>
+      {loading && <Loader></Loader>}
+      {!loading &&
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => (
+          <Post
+            key={post.id}
+            id={post.id}
+            user={post.user}
+            files={post.files}
+            likeCount={post.likeCount}
+            isLiked={post.isLiked}
+            comments={post.comments}
+            createdAt={post.createdAt}
+          ></Post>
+        ))}
+    </Wrapper>
+  );
 };
