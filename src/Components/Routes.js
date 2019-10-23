63807d6bebc9660314ba22ab77ca21dc6efbb87b
aispark <aispark@paran.com>
2019-10-23 20:35:54 +0900

    6-1 Header Logic part One

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -3,18 +3,20 @@ import PropTypes from "prop-types";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
+import Explore from "../Routes/Explore";
+import Search from "../Routes/Search";
+import Profile from "../Routes/Profile";
 
 const LoggedInRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Feed} />
-  </>
+    <Route path="/explore" component={Explore} />
+    <Route path="/search" component={Search} />
+    <Route path="/:username" component={Profile} />
+  </Switch>
 );
 
-const LoggedOutRoutes = () => (
-  <>
-    <Route exact path="/" component={Auth} />
-  </>
-);
+const LoggedOutRoutes = () => <Route exact path="/" component={Auth} />;
 
 const AppRouter = ({ isLoggedIn }) => (
   <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
