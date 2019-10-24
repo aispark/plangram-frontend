ba8a0ad4c887637a05c6e65055d1028f0e29b29e
aispark <aispark@paran.com>
2019-10-24 22:43:36 +0900

    7.4 toggleLike on Post Component

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -12,7 +12,7 @@ export default createGlobalStyle`
     background-color: ${props => props.theme.bgColor};
     color: ${props => props.theme.blackColor};
     font-size:14px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family:'Segoe UI';
     padding-top: 140px;
   }
 
