1bc03c8a4aa655aabd356bcc9bbbbff3340531a9
aispark <aispark@paran.com>
2019-10-24 22:03:09 +0900

    7.3 Post Component part Three

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import useInput from "../../Hooks/useInput";
 import PostPresenter from "./PostPresenter";
@@ -16,18 +16,34 @@ const PostContainer = ({
 }) => {
   const [isLikedS, setIsLiked] = useState(isLiked);
   const [likeCountS, setLikeCount] = useState(likeCount);
+  const [currentItem, setCurrentItem] = useState(0);
   const comment = useInput("");
+  const slide = () => {
+    const totalFiles = files.length;
+    if (currentItem === totalFiles - 1) {
+      setTimeout(() => setCurrentItem(0), 3000);
+    } else {
+      setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+    }
+  };
+  useEffect(() => {
+    slide();
+  }, [currentItem]);
+
   return (
     <PostPresenter
       user={user}
       files={files}
       likeCount={likeCountS}
+      location={location}
+      caption={caption}
       isLiked={isLikedS}
       comments={comments}
       createdAt={createdAt}
       newComment={comment}
       setIsLiked={setIsLiked}
       setLikeCount={setLikeCount}
+      currentItem={currentItem}
     />
   );
 };
@@ -57,7 +73,9 @@ PostContainer.propTypes = {
       }).isRequired
     })
   ).isRequired,
-  createdAt: PropTypes.string
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  createdAt: PropTypes.string.isRequired
 };
 
 export default PostContainer;
