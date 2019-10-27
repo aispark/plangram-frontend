83894fcf53ccaf7aae498a327b602571f6cfd5dd
aispark <aispark@paran.com>
2019-10-27 12:38:13 +0900

    9.1 Profile Screen part One

--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import TextareaAutosize from "react-autosize-textarea";
+import { Link } from "react-router-dom";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
 import { HeartFull, HeartEmpty, Comment as CommentIcon } from "../Icons";
@@ -11,6 +12,9 @@ const Post = styled.div`
   max-width: 600px;
   margin-bottom: 25px;
   user-select: none;
+  a {
+    color: inherit;
+  }
 `;
 
 const Header = styled.header`
@@ -112,13 +116,16 @@ export default ({
   toggleLike,
   onKeyPress,
   comments,
-  selfComments
+  selfComments,
+  caption
 }) => (
   <Post>
     <Header>
       <Avatar size="sm" url={avatar} />
       <UserColumn>
-        <FatText text={username} />
+        <Link to={username}>
+          <FatText text={username} />
+        </Link>
         <Location>{location}</Location>
       </UserColumn>
     </Header>
--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,1 +1,37 @@
-export default () => "Profile";
+import React from "react";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+import { withRouter } from "react-router-dom";
+import Loader from "../Components/Loader";
+
+const GET_USER = gql`
+  query seeUser($username: String!) {
+    seeUser(username: $username) {
+      id
+      avatar
+      username
+      fullName
+      isFollowing
+      isSelf
+      bio
+      followersCount
+      followingCount
+      posts {
+        id
+        files {
+          url
+        }
+        likeCount
+        commentCount
+      }
+      postsCount
+    }
+  }
+`;
+
+export default withRouter(({ match: { params: { username } } }) => {
+  console.log("username", username);
+  const { data, loading } = useQuery(GET_USER, { skip: true });
+  if (loading) return <Loader></Loader>;
+});
